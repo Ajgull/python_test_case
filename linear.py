45ba3677c22af90c4532fecb90061c724a9ef677
aigul <halitovaajgul210@gmail.com>
2026-02-22 18:09:51 +0500

    linear version done

---
+++ linear.py
@@ -0,0 +1,60 @@
+import time
+
+import requests
+
+import utils
+
+
+def linear_test_host(host: str, count: int) -> dict:
+    success_count = 0
+    fail_count = 0
+    error_count = 0
+    time_of_requests = []
+
+    for i in range(count):
+        try:
+            start_time = time.time()
+            response = requests.get(host, timeout=10)
+            end_time = time.time()
+            if 200 <= response.status_code < 400:
+                success_count += 1
+            else:
+                fail_count += 1
+
+            time_of_requests.append(round((end_time - start_time) * 1000, 3))
+
+        except requests.exceptions.RequestException as e:
+            print(f"Connection to host {host} {str(e)}")
+            error_count += 1
+
+    if time_of_requests:
+        min_time = round(min(time_of_requests), 3)
+        max_time = round(max(time_of_requests), 3)
+        avg_time = round(sum(time_of_requests) / len(time_of_requests), 3)
+    else:
+        min_time = max_time = avg_time = 0
+
+    return {
+        "Host": host,
+        "Success": success_count,
+        "Failed": fail_count,
+        "Errors": error_count,
+        "Min": min_time,
+        "Max": max_time,
+        "Avg": avg_time,
+    }
+
+
+def linear_test(hosts: list, count: int) -> tuple:
+    results = []
+    print("linear version")
+    start = time.time()
+    for host in hosts:
+        statistics = linear_test_host(host, count)
+        results.append(statistics)
+    end = time.time()
+
+    report_text = utils.format_statistics(results)
+    total_time = end - start
+
+    return report_text, total_time
---
+++ main.py
@@ -0,0 +1,74 @@
+import argparse
+import sys
+
+import utils
+from linear import linear_test
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="HTTP-server tester\n"
+        "–H/--hosts hosts separated by commas without spaces\n"
+        "–C/--count number of requests\n"
+        "–F/--file name of file to read hosts, each from a new line\n"
+        "–O/--output name of file for uploading results\n"
+        "-T/--type of program"
+    )
+
+    parser.add_argument("-H", "--hosts", required=False, help="Write the host")
+    parser.add_argument(
+        "-C",
+        "--count",
+        type=int,
+        default=1,
+        required=True,
+        help="Write count of requests",
+    )
+    parser.add_argument(
+        "-F", "--file", type=str, required=False, help="Write name of in_file"
+    )
+    parser.add_argument(
+        "-O", "--out", type=str, required=False, help="Write name of out_file"
+    )
+    parser.add_argument(
+        "-T",
+        "--type",
+        type=str,
+        required=True,
+        help="Write type of program to run (l - linear, p - parallel, a - async)",
+    )
+
+    args = parser.parse_args()
+    count = args.count
+    hosts = []
+
+    if args.hosts and args.file:
+        print("Impossible to run program with -F and -H keys")
+        sys.exit(1)
+
+    if not utils.is_positive_int_value(count):
+        parser.error("Wrong number of requests. It must be positive and integer")
+
+    if args.hosts:
+        hosts = args.hosts.split(",")
+    elif args.file:
+        hosts = utils.load_hosts_from_file(args.file)
+
+    hosts = utils.valid_urls(hosts)
+
+    if not hosts:
+        parser.error("Invalid type of host")
+
+    if args.type == "l":
+        report_text, total_time = linear_test(hosts, count)
+
+    if args.out:
+        utils.load_statistics_to_file(args.out, report_text)
+    else:
+        print(report_text)
+
+    print(f"total operating time of the program {total_time}")
+
+
+if __name__ == "__main__":
+    main()
